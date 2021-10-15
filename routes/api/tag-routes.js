c725c7a1fed09a929a6f2341053341b121450428
Nick Zabkar <nzabkar51@gmail.com>
2021-10-15 04:36:53 -0400

    Added delete route to tag routes file

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,27 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  let data = await Tag.findAll({
+    include: [
+      {model: Product, attributes: ["id", "product_name", "price", "stock", "category_id"]}
+    ]
+  });
+  res.json(data);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  let data = await Tag.findOne({
+    where: {id: req.params.id},
+    include: [
+      {model: Product, attributes: ['id', 'product_name', 'price', 'stock', 'category_id'], required: true}
+    ]
+  });
+  res.json(data);
 });
 
 router.post('/', (req, res) => {
@@ -21,8 +34,10 @@ router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  await Tag.destroy({where: {id: req.params.id}})
+  res.send("Tag removed");
 });
 
 module.exports = router;
